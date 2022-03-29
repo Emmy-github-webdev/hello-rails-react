6cd6bae2d6d90549fdb624aa750b0945593ed828
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-29 10:29:43 +0100

    Initialize greeting component

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
     @greeting = Greeting.all
-    render json: @greeting
+    render json: @greeting.to_json
   end
   
 end
--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,12 +1,17 @@
-import React from "react"
-import PropTypes from "prop-types"
-class App extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-      </React.Fragment>
-    );
-  }
+import React, {Fragment} from "react"
+import { BrowserRouter as Router, Route, Routes, Toute } from "react-router-dom"
+
+
+const App = () => {
+  return (
+    <Fragment>
+      <Router>
+        <Routes>
+          < Route exact path="/" element={< Greeting />}/>
+        </Routes>
+      </Router>
+    </Fragment>
+  )
 }
 
 export default App
